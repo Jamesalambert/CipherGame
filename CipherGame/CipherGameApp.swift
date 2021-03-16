fc7cea5bb6e5f7e2cc199f6d042adb1f74e14d70
James Lambert <james.a.lambert@gmail.com>
2021-03-16 22:13:52 +0000

    Initial Commit

---
+++ CipherGame/CipherGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CipherGameApp.swift
+//  CipherGame
+//
+//  Created by J Lambert on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CipherGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
