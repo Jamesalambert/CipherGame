2b54658137d4b3a803eaad075a6cc74d014b682f
James Lambert <james.a.lambert@gmail.com>
2021-04-05 00:39:59 +0100

    split ContentView into an extra file

--- CipherGame/ContentView + letter count.swift
+++ CipherGame/ContentView + letter count.swift
@@ -9,87 +9,85 @@ import SwiftUI
 
 extension ContentView {
     
-struct LetterCount : View {
-    
-    @EnvironmentObject
-    var viewModel : CipherPuzzle
-    var letterCount : [(character:Character, count:Int)]
-    
-    var body : some View {
+    struct LetterCount : View {
         
-        GeometryReader { geometry in
+        @EnvironmentObject
+        var viewModel : CipherPuzzle
+        
+        var letterCount : [(character:Character, count:Int)]
+        
+        var body : some View {
             
-            VStack {
-                //Divider()
-                Text("Character Count")
+            GeometryReader { geometry in
                 
-                ScrollView(.horizontal) {
-                    LazyVGrid(columns: self.columns(screenWidth: geometry.size.width), alignment: .center) {
-                        
-                        if letterCount.count > 0 {
+                VStack {
+                    //Divider()
+                    Text("Character Count")
+                    
+                    ScrollView(.horizontal) {
+                        LazyVGrid(columns: self.columns(screenWidth: geometry.size.width), alignment: .center) {
                             
-                            ForEach(0..<letterCount.count) { index in
-                                let cipherChar = letterCount[index].character
-                                PairCount(cipherChar: cipherChar,
-                                            plainChar: viewModel.plaintext(for: cipherChar),
-                                            count: letterCount[index].count)
+                            if letterCount.count > 0 {
+                                
+                                ForEach(0..<letterCount.count) { index in
+                                    let cipherChar = letterCount[index].character
+                                    PairCount(cipherChar: cipherChar,
+                                              plainChar: viewModel.plaintext(for: cipherChar),
+                                              count: letterCount[index].count)
+                                }
                             }
-                        }
-                    }.frame(minWidth: geometry.size.width) //centers the grid in the scrollview
-                    //.background(Color.blue)
-                }//.background(Color.green)
-            }//.background(Color.red)
+                        }.frame(minWidth: geometry.size.width) //centers the grid in the scrollview
+                        //.background(Color.blue)
+                    }//.background(Color.green)
+                }//.background(Color.red)
+            }
+        }
         
+        private
+        func columns(screenWidth : CGFloat) -> [GridItem] {
+            return Array(repeating: GridItem(.flexible(minimum: CGFloat(25), maximum: CGFloat(30))),
+                         count: 26)
         }
-    }
-    
-    
-    func columns(screenWidth : CGFloat) -> [GridItem] {
-        return Array(repeating: GridItem(.flexible(minimum: CGFloat(25), maximum: CGFloat(30))),
-                    count: 26)
-    }
-    
-}
         
-    
-struct PairCount : View {
-    
-    @EnvironmentObject
-    var viewModel : CipherPuzzle
-    
-    var cipherChar : Character
-    
-    var plainChar : Character?
-    
-    @Environment (\.colorScheme)
-    var colorScheme : ColorScheme
-    
-    var count : Int
-    
-    var body : some View {
-        VStack {
-            Group {
-                Text(String(cipherChar))
-                    
-                Text(count > 0 ? String(count) : "-").lineLimit(1)
-                    
-                Text(plainChar.string()).foregroundColor(Color.plaintext(for: colorScheme))
-            }.font(.system(.body, design: viewModel.fontDesign))
-            .textCase(viewModel.capType == 3 ? .uppercase : .lowercase)
-            .foregroundColor(foregroundColor)
-            
-            Spacer()
-        }
     }
     
-    var foregroundColor : Color? {
-        if count == 0 {
-            return Color.gray
-        } else if viewModel.currentCiphertextCharacter == cipherChar.lowerChar() {
-            return Color.highlightColor(for: colorScheme)
-        }
-        return Color.ciphertext(for: colorScheme)
-    }
     
-}
+    struct PairCount : View {
+        
+        @EnvironmentObject
+        var viewModel : CipherPuzzle
+        
+        var cipherChar : Character
+        var plainChar : Character?
+        var count : Int
+        
+        @Environment (\.colorScheme)
+        var colorScheme : ColorScheme
+        
+        var body : some View {
+            VStack {
+                Group {
+                    Text(String(cipherChar))
+                    
+                    Text(count > 0 ? String(count) : "-").lineLimit(1)
+                    
+                    Text(plainChar.string()).foregroundColor(Color.plaintext(for: colorScheme))
+                }.font(.system(.body, design: viewModel.fontDesign))
+                .textCase(viewModel.capType == 3 ? .uppercase : .lowercase)
+                .foregroundColor(foregroundColor)
+                
+                Spacer()
+            }
+        }
+        
+        private
+        var foregroundColor : Color? {
+            if count == 0 {
+                return Color.gray
+            } else if viewModel.currentCiphertextCharacter == cipherChar.lowerChar() {
+                return Color.highlightColor(for: colorScheme)
+            }
+            return Color.ciphertext(for: colorScheme)
+        }
+    }
 }
